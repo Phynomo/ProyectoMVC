f8a8e7fdc4e7e961c609ecac4986710a4471ab05
SRMAZ2023 <SRMAZ2020@gmail.com>
2023-02-21 11:44:15 -0600

    LAMAO

---
+++ ProyectoMVC/ProyectoMVC/Controllers/CargosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoMVC.Models;
+
+namespace ProyectoMVC.Controllers
+{
+    public class CargosController : Controller
+    {
+        private dbPedidosEncargosEntities db = new dbPedidosEncargosEntities();
+
+        // GET: Cargos
+        public ActionResult Index()
+        {
+            var tbCargos = db.tbCargos.Include(t => t.tbUsuarios).Include(t => t.tbUsuarios1);
+            return View(tbCargos.ToList());
+        }
+
+        // GET: Cargos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            if (tbCargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbCargos);
+        }
+
+        // GET: Cargos/Create
+        public ActionResult Create()
+        {
+            ViewBag.car_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario");
+            ViewBag.car_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario");
+            return View();
+        }
+
+        // POST: Cargos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "car_Id,car_Nombre,car_FechaCreacion,car_UsuarioCreacion,car_FechaModificacion,car_UsuarioModificacion,car_Estado")] tbCargos tbCargos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbCargos.Add(tbCargos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.car_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbCargos.car_UsuarioCreacion);
+            ViewBag.car_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbCargos.car_UsuarioModificacion);
+            return View(tbCargos);
+        }
+
+        // GET: Cargos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            if (tbCargos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.car_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbCargos.car_UsuarioCreacion);
+            ViewBag.car_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbCargos.car_UsuarioModificacion);
+            return View(tbCargos);
+        }
+
+        // POST: Cargos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "car_Id,car_Nombre,car_FechaCreacion,car_UsuarioCreacion,car_FechaModificacion,car_UsuarioModificacion,car_Estado")] tbCargos tbCargos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbCargos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.car_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbCargos.car_UsuarioCreacion);
+            ViewBag.car_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbCargos.car_UsuarioModificacion);
+            return View(tbCargos);
+        }
+
+        // GET: Cargos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            if (tbCargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbCargos);
+        }
+
+        // POST: Cargos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            db.tbCargos.Remove(tbCargos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoMVC/ProyectoMVC/Models/Municipio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoMVC.Models
+{
+
+    [MetadataType(typeof(tbMunicipiosMetaData))]
+
+    public partial class tbMunicipios
+    {
+
+    }
+
+    public class tbMunicipiosMetaData
+    {
+        
+        [Display(Name = "Id Municipio")]
+        public string mun_Id { get; set; }
+        [Display(Name = "Departamento")]
+        public string dep_Id { get; set; }
+        
+        
+        [Display(Name = "Municipio Nombre 20")]
+        [Required(ErrorMessage = "El campo {0} es necesario!")]
+        public string mun_Nombre { get; set; }
+    }
+}
