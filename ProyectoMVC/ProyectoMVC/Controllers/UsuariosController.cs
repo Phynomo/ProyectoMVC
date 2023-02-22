ec5849bdd46662c277ca7a044db6b2b5eed0ccdf
SRMAZ2023 <125476485+SRMAZ2023@users.noreply.github.com>
2023-02-21 20:31:52 -0600

    sql3

---
+++ ProyectoMVC/ProyectoMVC/Controllers/UsuariosController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoMVC.Models;
+
+namespace ProyectoMVC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private dbPedidosEncargosEntities db = new dbPedidosEncargosEntities();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            var tbUsuarios = db.tbUsuarios.Include(t => t.tbEmpleados2).Include(t => t.tbUsuarios2).Include(t => t.tbUsuarios3);
+            return View(tbUsuarios.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbUsuarios tbUsuarios = db.tbUsuarios.Find(id);
+            if (tbUsuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbUsuarios);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.emp_Id = new SelectList(db.tbEmpleados, "emp_Id", "emp_Nombre");
+            ViewBag.usu_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario");
+            ViewBag.usu_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "usu_Id,usu_Usuario,usu_Contrasenia,emp_Id,usu_UsuarioCreacion,usu_FechaCreacion,usu_UsuarioModificacion,usu_FechaModificacion,usu_Estado")] tbUsuarios tbUsuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbUsuarios.Add(tbUsuarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.emp_Id = new SelectList(db.tbEmpleados, "emp_Id", "emp_Nombre", tbUsuarios.emp_Id);
+            ViewBag.usu_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbUsuarios.usu_UsuarioCreacion);
+            ViewBag.usu_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbUsuarios.usu_UsuarioModificacion);
+            return View(tbUsuarios);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbUsuarios tbUsuarios = db.tbUsuarios.Find(id);
+            if (tbUsuarios == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.emp_Id = new SelectList(db.tbEmpleados, "emp_Id", "emp_Nombre", tbUsuarios.emp_Id);
+            ViewBag.usu_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbUsuarios.usu_UsuarioCreacion);
+            ViewBag.usu_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbUsuarios.usu_UsuarioModificacion);
+            return View(tbUsuarios);
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "usu_Id,usu_Usuario,usu_Contrasenia,emp_Id,usu_UsuarioCreacion,usu_FechaCreacion,usu_UsuarioModificacion,usu_FechaModificacion,usu_Estado")] tbUsuarios tbUsuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbUsuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.emp_Id = new SelectList(db.tbEmpleados, "emp_Id", "emp_Nombre", tbUsuarios.emp_Id);
+            ViewBag.usu_UsuarioCreacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbUsuarios.usu_UsuarioCreacion);
+            ViewBag.usu_UsuarioModificacion = new SelectList(db.tbUsuarios, "usu_Id", "usu_Usuario", tbUsuarios.usu_UsuarioModificacion);
+            return View(tbUsuarios);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbUsuarios tbUsuarios = db.tbUsuarios.Find(id);
+            if (tbUsuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbUsuarios);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbUsuarios tbUsuarios = db.tbUsuarios.Find(id);
+            db.tbUsuarios.Remove(tbUsuarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
